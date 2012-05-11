041cc75957da8277d04ad02e42370a48268fcae5
Roman Simecek <roman.simecek@awd.ch>
2012-05-11 19:09:10 +0200

    Seeds finished for EM 2012.

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,6 +48,167 @@ end
     place:      warschau,
     kickoff:    DateTime.strptime('08.06.2012 18:00', '%d.%m.%Y %H:%M'),
     group:      group_a
+  },
+  {
+    opponent_a: russia,
+    opponent_b: czech,
+    place:      breslau,
+    kickoff:    DateTime.strptime('08.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_a
+  },
+  {
+    opponent_a: greek,
+    opponent_b: czech,
+    place:      breslau,
+    kickoff:    DateTime.strptime('12.06.2012 18:00', '%d.%m.%Y %H:%M'),
+    group:      group_a
+  },
+  {
+    opponent_a: polen,
+    opponent_b: russia,
+    place:      warschau,
+    kickoff:    DateTime.strptime('12.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_a
+  },
+ {
+    opponent_a: czech,
+    opponent_b: polen,
+    place:      breslau,
+    kickoff:    DateTime.strptime('16.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_a
+  },
+  {
+    opponent_a: greek,
+    opponent_b: russia,
+    place:      warschau,
+    kickoff:    DateTime.strptime('16.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_a
+  },
+  {
+    opponent_a: nederland,
+    opponent_b: danemark,
+    place:      charkiw,
+    kickoff:    DateTime.strptime('09.06.2012 18:00', '%d.%m.%Y %H:%M'),
+    group:      group_b
+  },
+  {
+    opponent_a: germany,
+    opponent_b: portugal,
+    place:      lemberg,
+    kickoff:    DateTime.strptime('09.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_b
+  },
+  {
+    opponent_a: danemark,
+    opponent_b: portugal,
+    place:      lemberg,
+    kickoff:    DateTime.strptime('13.06.2012 18:00', '%d.%m.%Y %H:%M'),
+    group:      group_b
+  },
+  {
+    opponent_a: nederland,
+    opponent_b: germany,
+    place:      charkiw,
+    kickoff:    DateTime.strptime('13.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_b
+  },
+ {
+    opponent_a: portugal,
+    opponent_b: nederland,
+    place:      charkiw,
+    kickoff:    DateTime.strptime('17.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_b
+  },
+  {
+    opponent_a: danemark,
+    opponent_b: germany,
+    place:      lemberg,
+    kickoff:    DateTime.strptime('17.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_b
+  },
+  {
+    opponent_a: spain,
+    opponent_b: italy,
+    place:      danzig,
+    kickoff:    DateTime.strptime('10.06.2012 18:00', '%d.%m.%Y %H:%M'),
+    group:      group_c
+  },
+  {
+    opponent_a: irland,
+    opponent_b: croatia,
+    place:      posen,
+    kickoff:    DateTime.strptime('10.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_c
+  },
+  {
+    opponent_a: italy,
+    opponent_b: croatia,
+    place:      posen,
+    kickoff:    DateTime.strptime('14.06.2012 18:00', '%d.%m.%Y %H:%M'),
+    group:      group_c
+  },
+  {
+    opponent_a: spain,
+    opponent_b: irland,
+    place:      danzig,
+    kickoff:    DateTime.strptime('14.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_c
+  },
+ {
+    opponent_a: croatia,
+    opponent_b: spain,
+    place:      danzig,
+    kickoff:    DateTime.strptime('18.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_c
+  },
+  {
+    opponent_a: italy,
+    opponent_b: irland,
+    place:      posen,
+    kickoff:    DateTime.strptime('17.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_c
+  },
+  {
+    opponent_a: france,
+    opponent_b: england,
+    place:      donezk,
+    kickoff:    DateTime.strptime('11.06.2012 18:00', '%d.%m.%Y %H:%M'),
+    group:      group_d
+  },
+  {
+    opponent_a: ukraine,
+    opponent_b: sweden,
+    place:      kiew,
+    kickoff:    DateTime.strptime('11.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_d
+  },
+  {
+    opponent_a: ukraine,
+    opponent_b: france,
+    place:      donezk,
+    kickoff:    DateTime.strptime('15.06.2012 18:00', '%d.%m.%Y %H:%M'),
+    group:      group_d
+  },
+  {
+    opponent_a: sweden,
+    opponent_b: england,
+    place:      kiew,
+    kickoff:    DateTime.strptime('15.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_c
+  },
+ {
+    opponent_a: england,
+    opponent_b: ukraine,
+    place:      donezk,
+    kickoff:    DateTime.strptime('19.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_c
+  },
+  {
+    opponent_a: sweden,
+    opponent_b: france,
+    place:      kiew,
+    kickoff:    DateTime.strptime('19.06.2012 20:45', '%d.%m.%Y %H:%M'),
+    group:      group_c
   }
 ].each do |match|
   Match.create(match)
