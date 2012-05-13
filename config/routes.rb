db2e92e37ab4403fda53fa683bf9104e5e82e7fb
Roman Simecek <roman.simecek@awd.ch>
2012-05-14 01:24:24 +0200

    Matches routes added.

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ SportBets::Application.routes.draw do
   resources :groups, only: [:index, :show] do
     resources :group_bets, expect: [:show]
     resources :teams, only: [:index]
-
+    resources :matches
     member do
       get :finish
     end
   end
-  resources :matches
   resources :group_bets
+  resources :matches
   root :to => 'groups#index'
 end
