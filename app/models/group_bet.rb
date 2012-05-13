9e6cbc0dd0fdc16ad9b5ba4ce8ef902e5f95235a
Roman Simecek <roman.simecek@awd.ch>
2012-05-13 14:06:57 +0200

    Group bet has a participant.

--- app/models/group_bet.rb
+++ app/models/group_bet.rb
@@ -1,8 +1,7 @@
 class GroupBet
   include Mongoid::Document
 
-  field :name, type: String
-
+  belongs_to :participant
   belongs_to :group
   belongs_to :first_place,  class_name: 'Team', inverse_of: :bet_first_place
   belongs_to :second_place, class_name: 'Team', inverse_of: :bet_second_place
