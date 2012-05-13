30d11c0d36634e0ebf9eddeb0a338b81c2c9a240
Roman Simecek <roman.simecek@awd.ch>
2012-05-13 19:50:15 +0200

    Validations added.

--- app/models/group_bet.rb
+++ app/models/group_bet.rb
@@ -5,4 +5,13 @@ class GroupBet
   belongs_to :group
   belongs_to :first_place,  class_name: 'Team', inverse_of: :bet_first_place
   belongs_to :second_place, class_name: 'Team', inverse_of: :bet_second_place
+
+  validates_presence_of :first_place
+  validates_presence_of :second_place
+  validates_presence_of :group
+  validates_presence_of :participant
+
+  def name
+    "#{participant.first_name} #{participant.last_name}" if participant
+  end
 end
