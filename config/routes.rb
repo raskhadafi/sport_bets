6bde3b2212de4621b13ef13618ac218ec7f61775
Roman Simecek <roman.simecek@awd.ch>
2012-05-13 19:50:55 +0200

    View optimized and group finish route added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 SportBets::Application.routes.draw do
   resources :participants
   resources :groups, only: [:index, :show] do
-    resources :group_bets
+    resources :group_bets, expect: [:show]
     resources :teams, only: [:index]
+
+    member do
+      post :finish
+    end
   end
   resources :matches
   resources :group_bets
