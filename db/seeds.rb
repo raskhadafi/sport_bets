6a445eae08892381b5a6354986875a9d6a4a1e09
Roman Simecek <roman.simecek@awd.ch>
2012-05-14 08:01:22 +0200

    Transfers in the seed corrected.

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,27 +28,32 @@ posen    = Place.create!(name: 'Posen')
 donezk   = Place.create!(name: 'Donezk')
 kiew     = Place.create!(name: 'Kiew')
 
-quater  = Group.create!(name: 'Viertelfinal', round: 2)
-half    = Group.create!(name: 'Halbfinal',    round: 3)
-final   = Group.create!(name: 'Final',        round: 4)
+final      = Group.create!(name: 'Final', round: 4)
+final_game = Match.create!(group: final, place: kiew, kickoff: DateTime.strptime('08.06.2012 18:00', '%d.%m.%Y %H:%M'))
 
-quater_match_one   = Match.create!(group: quater, place: kiew, kickoff: DateTime.strptime('08.06.2012 18:00', '%d.%m.%Y %H:%M'))
-quater_match_two   = Match.create!(group: quater, place: kiew, kickoff: DateTime.strptime('08.06.2012 18:00', '%d.%m.%Y %H:%M'))
-quater_match_three = Match.create!(group: quater, place: kiew, kickoff: DateTime.strptime('08.06.2012 18:00', '%d.%m.%Y %H:%M'))
-quater_match_four  = Match.create!(group: quater, place: kiew, kickoff: DateTime.strptime('08.06.2012 18:00', '%d.%m.%Y %H:%M'))
+half           = Group.create!(name: 'Halbfinal', round: 3)
+half_match_one = Match.create!(group: half, place: kiew, kickoff: DateTime.strptime('08.06.2012 18:00', '%d.%m.%Y %H:%M'), next_match: final_game, next_position: 'opponent_a')
+half_match_two = Match.create!(group: half, place: kiew, kickoff: DateTime.strptime('08.06.2012 18:00', '%d.%m.%Y %H:%M'), next_match: final_game, next_position: 'opponent_b')
+
+quater             = Group.create!(name: 'Viertelfinal', round: 2)
+quater_match_one   = Match.create!(group: quater, place: warschau, kickoff: DateTime.strptime('21.06.2012 20:45', '%d.%m.%Y %H:%M'), next_match: half_match_one, next_position: 'opponent_a')
+quater_match_two   = Match.create!(group: quater, place: danzig,   kickoff: DateTime.strptime('22.06.2012 20:45', '%d.%m.%Y %H:%M'), next_match: half_match_two, next_position: 'opponent_a')
+quater_match_three = Match.create!(group: quater, place: donezk,   kickoff: DateTime.strptime('23.06.2012 20:45', '%d.%m.%Y %H:%M'), next_match: half_match_one, next_position: 'opponent_b')
+quater_match_four  = Match.create!(group: quater, place: kiew,     kickoff: DateTime.strptime('24.06.2012 20:45', '%d.%m.%Y %H:%M'), next_match: half_match_two, next_position: 'opponent_b')
+
 
 group_a = Group.create!(name: 'Gruppe A',
                        round: 1,
                        first_next_match: quater_match_one,
                        first_next_match_field: 'opponent_a',
                        second_next_match: quater_match_two,
-                       second_next_match_field: 'opponent_a'
+                       second_next_match_field: 'opponent_b'
                        )
 group_b = Group.create!(name: 'Gruppe B',
                        round: 1,
-                       first_next_match: quater_match_one,
-                       first_next_match_field: 'opponent_b',
-                       second_next_match: quater_match_two,
+                       first_next_match: quater_match_two,
+                       first_next_match_field: 'opponent_a',
+                       second_next_match: quater_match_one,
                        second_next_match_field: 'opponent_b'
                        )
 group_c = Group.create!(name: 'Gruppe C',
@@ -56,13 +61,13 @@ group_c = Group.create!(name: 'Gruppe C',
                        first_next_match: quater_match_three,
                        first_next_match_field: 'opponent_a',
                        second_next_match: quater_match_four,
-                       second_next_match_field: 'opponent_a'
+                       second_next_match_field: 'opponent_b'
                        )
 group_d = Group.create!(name: 'Gruppe D',
                        round: 1,
-                       first_next_match: quater_match_three,
-                       first_next_match_field: 'opponent_b',
-                       second_next_match: quater_match_four,
+                       first_next_match: quater_match_four,
+                       first_next_match_field: 'opponent_a',
+                       second_next_match: quater_match_three,
                        second_next_match_field: 'opponent_b'
                        )
 
