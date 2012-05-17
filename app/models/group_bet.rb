33a5d2ac042f10068f26fb12737f672e675f32b9
Roman Simecek <roman.simecek@awd.ch>
2012-05-17 17:26:59 +0200

    Group bet belongs to user.

--- app/models/group_bet.rb
+++ app/models/group_bet.rb
@@ -1,7 +1,9 @@
 class GroupBet
   include Mongoid::Document
 
-  belongs_to :participant
+  Win = 2
+
+  belongs_to :user
   belongs_to :group
   belongs_to :first_place,  class_name: 'Team', inverse_of: :bet_first_place
   belongs_to :second_place, class_name: 'Team', inverse_of: :bet_second_place
