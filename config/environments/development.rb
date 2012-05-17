ff22edb4334aa5af81e7d3b3a1fc09f8f95234df
Roman Simecek <roman.simecek@awd.ch>
2012-05-17 15:40:15 +0200

    Smtp server for development set.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,9 +27,11 @@ SportBets::Application.configure do
   config.assets.debug = true
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = {host: '127.0.0.1:1025'}
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'localhost', :port => 1025 }
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: "utf-8"
