ee9e1e4894895e4fbe3577c6e85274cf04dec47e
Roman Simecek <roman.simecek@awd.ch>
2012-05-27 18:15:22 +0200

    Group bets link are now shown when not made.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,23 @@ module ApplicationHelper
   def team_collection(group)
     group.matches.map { |e| e.opponent_a  }.uniq
   end
+
+  def group_bets_links(group_bets)
+    existing_group_ids = group_bets.map(&:group).map(&:id)
+    links = ''
+
+    Group.start.each do |group|
+      next if existing_group_ids.include?(group.id)
+
+      links += content_tag :li do
+        link_to group, new_group_group_bet_path(group)
+      end
+    end
+
+    links.html_safe
+  end
+
+  def additional_bets_links(additional_bets)
+    "buhu"
+  end
 end
